907cbf5f60fb36cc4e5dc5ec62a18e7e15503314
huangjipeng <1319639755@qq.com>
2022-03-28 17:59:31 +0800

    feat: d.ts 使用

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,22 @@
 import { FunctionComponent } from "react";
+import myFetch from "./fetch";
+
+myFetch.get('/api').then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log('err', err)
+})
+
 
 type TestProps = {
   title: string;
   desc: string;
 }
+
+myFetch<TestProps>('api', 'GET').then(res => {
+  
+})
+
 
 const Test = (props: TestProps) => {
   return (
